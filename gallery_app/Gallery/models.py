02b6f89531f7504448f0a474f6b4bba48f2acde4
MaryAnne45 <mary.jereh@gmail.com>
2018-05-06 23:35:10 +0300

    added a dynamic link

--- gallery_app/Gallery/models.py
+++ gallery_app/Gallery/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category_name=models.CharField(max_length = 60)
+    name=models.CharField(max_length = 60)
 class Image(models.Model):
     name=models.CharField(max_length=30)
     image_description=models.TextField()
--- gallery_app/Gallery/urls.py
+++ gallery_app/Gallery/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
 url(r'^$',views.start,name='Start'),
-url(r'^image/(\d+)',views.image,name='Image')
+url(r'^image/(\d+)',views.image,name='description'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
