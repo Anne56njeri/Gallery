8d59dd6e1843d0e333a8c1917e8707c08d8a80aa
MaryAnne45 <mary.jereh@gmail.com>
2018-05-06 22:01:47 +0300

    added an image column

--- gallery_app/Gallery/models.py
+++ gallery_app/Gallery/models.py
@@ -6,4 +6,5 @@ class Category(models.Model):
 class Image(models.Model):
     name=models.CharField(max_length=30)
     image_description=models.TextField()
+    image_path=models.ImageField(upload_to='images/',blank=True)
     category=models.ForeignKey(Category,null=True)
