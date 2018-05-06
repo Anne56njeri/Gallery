c937675027ffbbd8370a33b81f8b6e2f47c77665
MaryAnne45 <mary.jereh@gmail.com>
2018-05-06 22:15:57 +0300

    dispalying images

--- gallery_app/Gallery/views.py
+++ gallery_app/Gallery/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def start(request):
     title='welcome to Gallery'
-    return render(request,'all-images/first.html',{"title":title})
+    my_images=Image.objects.all()
+    return render(request,'all-images/first.html',{"title":title,"my_images":my_images})
