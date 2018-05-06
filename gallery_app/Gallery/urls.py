12143fb475431ce39ea895e5cb094a0d8f2cedfb
MaryAnne45 <mary.jereh@gmail.com>
2018-05-06 22:58:22 +0300

    added a page to display image details

--- gallery_app/Gallery/urls.py
+++ gallery_app/Gallery/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
 url(r'^$',views.start,name='Start'),
+url(r'^image/(\d+)',views.image,name='Image')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- gallery_app/Gallery/views.py
+++ gallery_app/Gallery/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Image
+from django.http import Http404
 
 # Create your views here.
 def start(request):
     title='welcome to Gallery'
     my_images=Image.objects.all()
     return render(request,'all-images/first.html',{"title":title,"my_images":my_images})
+def image(request,image_id):
+    try:
+        images=Image.objects.get(id=image_id)
+    except DoesNotExsist:
+        raise Http404()
+    return render(request,"all-images/image.html",{"images":images})
