1dc901c0bdba51873941aae92bad260f14ee5a1a
MaryAnne45 <mary.jereh@gmail.com>
2018-05-07 11:30:06 +0300

    added a drop down for locations

--- gallery_app/Gallery/urls.py
+++ gallery_app/Gallery/urls.py
@@ -7,7 +7,11 @@ urlpatterns=[
 url(r'^$',views.start,name='Start'),
 url(r'^image/(\d+)',views.image,name='description'),
 url(r'^search/',views.search_category,name='search_category'),
-url(r'^location/',views.london,name='location'),
+url(r'^London/',views.london,name='location'),
+url(r'^China/',views.china,name='location1'),
+url(r'^Malawi/',views.malawi,name='location2'),
+url(r'^Europe/',views.europe,name='location3'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
