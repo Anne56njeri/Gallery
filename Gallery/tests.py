b667d42b4bf7644a12382f9551498f901951387e
MaryAnne45 <mary.jereh@gmail.com>
2018-05-07 23:29:03 +0300

    added test to check instance is created

--- Gallery/tests.py
+++ Gallery/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Category,Location,Images
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.one=Category(name='Travel')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.one,Category))
